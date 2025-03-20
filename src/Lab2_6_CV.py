28d8d38bba655901a82b0778f22bbf472479407a
luiscs03083 <luiscs0308@gmail.com>
2025-03-20 19:02:25 +0100

    final

--- src/Lab2_6_CV.py
+++ src/Lab2_6_CV.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def cross_validation(model, X, y, nFolds):
     """
     Perform cross-validation on a given machine learning model to evaluate its performance.
@@ -44,26 +46,32 @@ def cross_validation(model, X, y, nFolds):
         # Implement Leave One Out CV
         nFolds = X.shape[0]
 
-    # TODO: Calculate fold_size based on the number of folds
-    fold_size = None
+    # Calculate fold_size based on the number of folds
+    fold_size =X.shape[0] // nFolds
 
-    # TODO: Initialize a list to store the accuracy values of the model for each fold
+    # Initialize a list to store the accuracy values of the model for each fold
     accuracy_scores = []
 
     for i in range(nFolds):
-        # TODO: Generate indices of samples for the validation set for the fold
-        valid_indices = None
+        # Generate indices of samples for the validation set for the fold
+        valid_indices = list(range(i * fold_size, (i + 1) * fold_size))
+        
+        # Generate indices of samples for the training set for the fold
+        train_indices = list(set(range(X.shape[0])) - set(valid_indices))
 
-        # TODO: Generate indices of samples for the training set for the fold
-        train_indices = None
+        # Dividir el dataset en entrenamiento y validación
+        X_train, X_valid = X[train_indices], X[valid_indices]
+        y_train, y_valid = y[train_indices], y[valid_indices]
 
-        # TODO: Split the dataset into training and validation
-        X_train, X_valid = None, None
-        y_train, y_valid = None, None
+        # Entrenar el modelo
+        model.fit(X_train, y_train)
 
-        # TODO: Train the model with the training set
+        # Evaluar el modelo y almacenar la métrica de rendimiento
+        score = model.score(X_valid, y_valid)
+        accuracy_scores.append(score)
 
-        # TODO: Calculate the accuracy of the model with the validation set and store it in accuracy_scores
+    # Calcular media y desviación estándar de las puntuaciones obtenidas
+    mean_score = np.mean(accuracy_scores)
+    std_score = np.std(accuracy_scores)
 
-    # TODO: Return the mean and standard deviation of the accuracy_scores
-    return None, None
+    return mean_score, std_score
